191a0460e1ef7c6051defb470e5c35bd7f2eefcf
elsagomez <elsa.ghdz@gmail.com>
2017-04-26 17:16:13 -0400

    first commmit

---
+++ clozeCard.js
@@ -0,0 +1,34 @@
+export.clozeQuestions = [
+{
+	full: "Sean Spicer is the current Press Secretary of the US White House"
+	cloze: "Spicer"
+}
+{
+	full: "Carlos Slim is the 6th richest man in the world, and the richest in Mexico"
+	cloze: "Slim"
+}
+{
+	full: "Nelson Mandela was president of South Africa in 1994 (year)"
+	cloze: "1994"
+}
+{
+	full: "Christopher Columbus land in America in 1492 (year)"
+	cloze: "1492"
+}
+{
+	full: "Ancient Egypt was conquered in 10 BC by the Roman empire?"
+	cloze: "Roman"
+}
+{
+	full: "Canada is Americas northern neighbor"
+	front: "Canada"
+}
+{
+	full: "The Atlantic Ocean in the East Coast of the US"
+	front: "Atlantic"
+}
+{
+	full: "Snowhite is the first disney princess"
+	cloze: "Snowhite"
+}
+]
